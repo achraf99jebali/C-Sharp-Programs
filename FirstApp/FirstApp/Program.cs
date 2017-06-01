b1d453486eacefd1c0d79ef379108a8e3065602c
achraf99jebali <achrafjebali16@gmail.com>
2017-06-01 12:04:08 +0100

    this is my first program in C#

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first app in C#
+            Console.WriteLine("Hello, World");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Integer_Variable/Integer_Variable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter the first nember : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second nember :");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"the sum of the nembers is {a + b}");
+            Console.ReadKey();
+        }
+    }
+}
