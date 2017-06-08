f10400efb80c39c8da643657dec22bcf7e59a133
achraf99jebali <achrafjebali16@gmail.com>
2017-06-08 15:23:40 +0100

    length of array

---
+++ ArrayOfString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFullname = { "Achraf", "Jebali" };
+            int myArrayLength = myFullname.Length;
+            for (int i = 0; i<myArrayLength;i++)
+            {
+                Console.Write($"{myFullname[i]}"+" ");
+            }
+            Console.WriteLine($"\nthe length of my array is {myArrayLength}");
+            Console.ReadKey();
+        }
+    }
+}
